8eda922fd076c91c58e235cabd25b410c33db6ea
camlo01 <camilobeltran174@gmail.com>
2022-05-26 12:49:41 -0500

    prepared to add view

--- src/main/java/com/cats/gatos_app/Gatos_app.java
+++ src/main/java/com/cats/gatos_app/Gatos_app.java
@@ -4,10 +4,9 @@ package com.cats.gatos_app;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
-
 import com.cats.gatos_app.Model.Gatos;
 import com.cats.gatos_app.Controller.GatosService;
+import com.cats.gatos_app.View.Inicio;
 import java.io.IOException;
 import javax.swing.JOptionPane;
 
@@ -18,6 +17,8 @@ import javax.swing.JOptionPane;
 public class Gatos_app {
 
     public static void main(String[] args) throws IOException {
+//        Componente inicio
+
         System.out.println("Aplicacion iniciada");
 
         String[] botones = {"1. ver gatos", "2. Ver favoritos", "3. Salir"};
@@ -26,8 +27,10 @@ public class Gatos_app {
         do {
             //Menu principal
             String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu principal", JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]);
+//            Inicio inicio = new Inicio();
+//            inicio.setVisible(true);
 
-            //Validar seleccion usuario
+//Validar seleccion usuario
             for (int i = 0; i < botones.length; i++) {
                 if (opcion.equals(botones[i])) {
                     opcion_menu = i;
